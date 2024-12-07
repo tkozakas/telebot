2cde1283eb13ce201fd2dc7630325ec9e3026fd2
tkozakas <tomas.kozakas@gmail.com>
2024-12-08 00:44:36 +0200

    Minor refactor

--- src/main/kotlin/com/telebot/handler/RandomResponseHandler.kt
+++ src/main/kotlin/com/telebot/handler/RandomResponseHandler.kt
@@ -2,14 +2,13 @@ package com.telebot.handler
 
 import com.telebot.enums.Command
 import com.telebot.service.ChatService
-import com.telebot.service.CommandService
 import com.telebot.service.FactService
 import com.telebot.service.StickerService
 import io.github.dehuckakpyt.telegrambot.TelegramBot
 import io.github.dehuckakpyt.telegrambot.model.telegram.Chat
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
-import java.util.*
+import kotlin.random.Random
 
 @Service
 class RandomResponseHandler(
@@ -18,37 +17,20 @@ class RandomResponseHandler(
     private val stickerService: StickerService,
     private val chatService: ChatService
 ) {
-    private val random = Random()
 
-    private fun shouldTriggerRandomResponse(): Boolean {
-        return random.nextDouble() < randomResponseChance
-    }
+    private fun shouldTriggerRandomResponse() = Random.nextDouble() < randomResponseChance
 
-    private fun selectRandomHandler(): CommandService {
-        val handlers = listOf(
-            factService,
-            stickerService
-        )
-        val index = random.nextInt(handlers.size)
-        return handlers[index]
-    }
+    private fun selectRandomHandler() = listOf(factService, stickerService).random()
 
     suspend fun handle(telegramChat: Chat, telegramBot: TelegramBot, text: String?) {
-        if (Command.isCommand(text)) {
-            return
-        }
-        if (!shouldTriggerRandomResponse()) {
-            return
-        }
-        val chat = chatService.saveChat(telegramChat.id, telegramChat.title)
-        if (chat.id == null) {
-            return
-        }
+        if (Command.isCommand(text) || !shouldTriggerRandomResponse()) return
 
+        val chat = chatService.saveChat(telegramChat.id, telegramChat.title) ?: return
         val bot = TelegramBotActions(chatId = chat.telegramChatId!!, bot = telegramBot, input = null)
-        when (selectRandomHandler()) {
-            is FactService -> factService.provideRandomFact(bot);
-            is StickerService -> stickerService.sendRandomSticker(chat, bot);
+
+        when (val handler = selectRandomHandler()) {
+            is FactService -> handler.provideRandomFact(bot)
+            is StickerService -> handler.sendRandomSticker(chat, bot)
         }
     }
 }

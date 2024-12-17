ad0d88a4a06e3618cdf24f8fca5a2f77d9557f5d
tkozakas <tomas.kozakas@gmail.com>
2024-12-17 11:49:38 +0200

    Minor fixes

--- src/main/kotlin/com/telebot/handler/CommandHandler.kt
+++ src/main/kotlin/com/telebot/handler/CommandHandler.kt
@@ -9,7 +9,6 @@ import eu.vendeli.tgbot.api.message.sendMessage
 import eu.vendeli.tgbot.types.ParseMode
 import eu.vendeli.tgbot.types.User
 import eu.vendeli.tgbot.types.internal.ProcessedUpdate
-import eu.vendeli.tgbot.types.internal.UpdateType
 import org.springframework.stereotype.Component
 
 @Component
@@ -24,38 +23,38 @@ class CommandHandler(
     private val updateContextFactory: UpdateContextFactory
 ) {
 
-    @CommonHandler.Regex("^${Commands.GPT}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.GPT}$")
     suspend fun handleGpt(update: ProcessedUpdate, bot: TelegramBot) {
         gptService.handle(updateContextFactory.create(update, bot))
     }
 
-    @CommonHandler.Regex("^${Commands.MEME}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.MEME}$")
     suspend fun handleMeme(update: ProcessedUpdate, bot: TelegramBot) {
         memeService.handle(updateContextFactory.create(update, bot))
     }
 
-    @CommonHandler.Regex("^${Commands.STICKER}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.STICKER}$")
     suspend fun handleSticker(update: ProcessedUpdate, bot: TelegramBot) {
         stickerService.handle(updateContextFactory.create(update, bot))
     }
 
-    @CommonHandler.Regex("^${Commands.FACT}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.FACT}$")
     suspend fun handleFact(update: ProcessedUpdate, bot: TelegramBot) {
         factService.handle(updateContextFactory.create(update, bot))
     }
 
-    @CommonHandler.Regex("^${Commands.TTS}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.TTS}$")
     suspend fun handleTts(update: ProcessedUpdate, bot: TelegramBot) {
         ttsService.handle(updateContextFactory.create(update, bot))
     }
 
-    @CommonHandler.Regex("^${Commands.DAILY_MESSAGE}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.DAILY_MESSAGE}$")
     suspend fun handleDailyMessage(update: ProcessedUpdate, bot: TelegramBot) {
         dailyMessageService.handle(updateContextFactory.create(update, bot))
     }
 
-    @CommonHandler.Regex("^${Commands.HELP}|${Commands.START}$", scope = [UpdateType.MESSAGE])
+    @CommonHandler.Regex("^${Commands.HELP}|${Commands.START}$")
     suspend fun handleHelp(user: User, bot: TelegramBot) {
-        sendMessage { printerUtil.printHelp() }.options { parseMode = ParseMode.Markdown }.send(user, bot)
+        sendMessage { printerUtil.printHelp() }.options { parseMode = ParseMode.Markdown }.send(user.id, bot)
     }
 }

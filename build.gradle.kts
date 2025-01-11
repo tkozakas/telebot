decc87fbfbc44e4199fe0664ddd9b46377efd397
tkozakas <tomas.kozakas@gmail.com>
2025-01-11 17:22:27 +0200

    Update versions for dependencies including spring

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ val javaTarget = JavaVersion.VERSION_21
 
 plugins {
     kotlin("plugin.jpa") version "2.1.0"
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    kotlin("plugin.serialization") version "1.8.21"
-    id("org.springframework.boot") version "3.3.0"
-    id("io.spring.dependency-management") version "1.1.6"
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.spring") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.7"
     id("com.google.devtools.ksp") version "2.1.0-1.0.29"
     id("eu.vendeli.telegram-bot") version "7.8.0"
 }
@@ -30,18 +30,18 @@ repositories {
 
 dependencies {
     implementation("eu.vendeli:spring-ktgram-starter:7.8.0")
-
-    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.1.4")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign:4.2.0")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j:3.2.0")
 
-    implementation("org.hibernate:hibernate-core:6.6.3.Final")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.postgresql:postgresql:42.7.4")
-
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
     implementation("com.arthenica:ffmpeg-kit-full-gpl:4.5.1-1")
-    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j:3.0.4")
+
+    implementation("ch.qos.logback:logback-classic:1.5.15")
+    implementation("ch.qos.logback:logback-core:1.5.15")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
--- src/main/kotlin/com/telebot/client/GptClient.kt
+++ src/main/kotlin/com/telebot/client/GptClient.kt
@@ -3,12 +3,10 @@ package com.telebot.client
 import com.telebot.dto.GptRequestDTO
 import com.telebot.dto.GptResponseDTO
 import org.springframework.cloud.openfeign.FeignClient
-import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 
-@Component
 @FeignClient(name = "gptClient", url = "https://api.groq.com/openai/v1")
 interface GptClient {
 

99fe80447e17cd9d10806d96d7719ec081714413
tkozakas <tomas.kozakas@gmail.com>
2025-02-24 12:34:08 +0200

    Refactor model

--- src/main/kotlin/com/telebot/repository/UserRepository.kt
+++ src/main/kotlin/com/telebot/repository/UserRepository.kt
@@ -2,8 +2,6 @@ package com.telebot.repository
 
 import com.telebot.model.User
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
-    fun findByUserId(userId: Long, chatId: Long): Optional<User>
 }

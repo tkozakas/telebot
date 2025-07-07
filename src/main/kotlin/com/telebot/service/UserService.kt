6fbc80e69f7a9e941c5cfce8b9796ca1ccab5c2f
tkozakas <tomas.kozakas@gmail.com>
2025-07-07 22:34:21 +0300

    Refactor ChatService and UserService to enhance chat and user creation logic with transactional support

--- src/main/kotlin/com/telebot/service/UserService.kt
+++ src/main/kotlin/com/telebot/service/UserService.kt
@@ -4,16 +4,24 @@ import com.telebot.model.Chat
 import com.telebot.model.User
 import com.telebot.repository.UserRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UserService(
     private val userRepository: UserRepository
 ) {
-    fun saveUser(user: User): User {
-        return userRepository.save(user)
+    @Transactional
+    fun findOrCreate(userId: Long, username: String): User {
+        return userRepository.findById(userId).orElseGet {
+            val newUser = User(userId, username)
+            userRepository.save(newUser)
+        }
     }
 
-    fun findUsersByChat(chat: Chat): MutableList<User> {
-        return userRepository.findAllByChat(chat)
+    @Transactional(readOnly = true)
+    fun findRandomUserByChat(chat: Chat): User {
+        return userRepository.findRandomUserByChatId(chat.chatId).orElseGet {
+            throw IllegalArgumentException("No users found in chat with id ${chat.chatId}")
+        }
     }
 }

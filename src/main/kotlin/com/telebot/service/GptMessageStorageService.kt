5229e2531f995d588484fce95a1613692f9b0b5d
tkozakas <tomas.kozakas@gmail.com>
2025-07-19 10:36:05 +0300

    Refactor GptService, MemeService, StickerService, and TtsService to improve structure and remove unused code

--- src/main/kotlin/com/telebot/service/GptMessageStorageService.kt
+++ src/main/kotlin/com/telebot/service/GptMessageStorageService.kt
@@ -4,6 +4,7 @@ import com.telebot.dto.GptRequestDTO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
+// TODO redis cache
 @Service
 class GptMessageStorageService {
 
--- src/main/kotlin/com/telebot/service/GptService.kt
+++ src/main/kotlin/com/telebot/service/GptService.kt
@@ -1,18 +1,9 @@
 package com.telebot.service
 
 import com.telebot.client.GptClient
-import com.telebot.dto.GptRequestDTO
-import com.telebot.enums.SubCommand
 import com.telebot.model.UpdateContext
 import com.telebot.properties.GptProperties
-import eu.vendeli.tgbot.api.media.sendDocument
-import eu.vendeli.tgbot.api.message.sendMessage
-import eu.vendeli.tgbot.types.internal.ImplicitFile
-import eu.vendeli.tgbot.types.internal.InputFile
 import org.springframework.stereotype.Service
-import kotlin.io.path.createTempFile
-import kotlin.io.path.readBytes
-import kotlin.io.path.writeText
 
 @Service
 class GptService(
@@ -31,56 +22,10 @@ class GptService(
     override suspend fun handle(update: UpdateContext) {
         val prompt = update.args.drop(1).joinToString(" ")
 
-        when (update.subCommand?.lowercase()) {
-            SubCommand.MEMORY.name.lowercase() -> sendChatHistory(update)
-            SubCommand.FORGET.name.lowercase() -> clearChatHistory(update)
-            else -> processChat(update, prompt)
-        }
+//        when (update.subCommand?.lowercase()) {
+//            SubCommand.MEMORY.name.lowercase() -> sendChatHistory(update)
+//            SubCommand.FORGET.name.lowercase() -> clearChatHistory(update)
+//            else -> processChat(update, prompt)
+//        }
     }
-
-    private suspend fun sendChatHistory(update: UpdateContext) {
-        val historyFile = generateChatHistory(update.telegramChatId)
-        if (historyFile == null) {
-            sendMessage { CHAT_HISTORY_EMPTY }.send(update.telegramChatId, update.bot)
-        } else {
-            sendDocument(ImplicitFile.InpFile(historyFile))
-                .caption { "Chat history" }
-                .send(update.telegramChatId, update.bot)
-        }
-    }
-
-    private suspend fun clearChatHistory(update: UpdateContext) {
-        gptMessageStorageService.clearMessages(update.telegramChatId)
-        sendMessage { CHAT_HISTORY_CLEARED }.send(update.telegramChatId, update.bot)
-    }
-
-    private suspend fun processChat(update: UpdateContext, prompt: String) {
-        if (prompt.isBlank()) {
-            sendMessage { INVALID_PROMPT }.send(update.telegramChatId, update.bot)
-            return
-        }
-        val response = processPrompt(update.telegramChatId, prompt)
-        sendMessage { response ?: NO_RESPONSE }.send(update.telegramChatId, update.bot)
-    }
-
-    private fun processPrompt(chatId: Long, prompt: String): String? {
-        val userMessage = GptRequestDTO.Message(role = "user", content = prompt)
-        gptMessageStorageService.addMessage(chatId, userMessage)
-
-        val messages = gptMessageStorageService.getMessages(chatId)
-        val gptResponse =
-            gptClient.getChatCompletion("Bearer ${gptProperties.token}", GptRequestDTO(messages, gptProperties))
-        val botMessage = gptResponse?.choices?.firstOrNull()?.message?.let {
-            GptRequestDTO.Message(role = "assistant", content = it.content)
-        }
-        botMessage?.let { gptMessageStorageService.addMessage(chatId, it) }
-        return botMessage?.content
-    }
-
-    private fun generateChatHistory(chatId: Long) =
-        gptMessageStorageService.getMessages(chatId).takeIf { it.isNotEmpty() }?.let {
-            InputFile(createTempFile(prefix = "chat_history_", suffix = ".txt").apply {
-                writeText(it.joinToString("\n") { msg -> "${msg.role}: ${msg.content}" })
-            }.readBytes())
-        }
 }
